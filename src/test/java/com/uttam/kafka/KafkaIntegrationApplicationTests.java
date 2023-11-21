76771939fab373b75d1889b228268448732ee0c7
Uttam Bhat <uttambhat1711@gmail.com>
2023-11-21 12:58:23 -0500

    springboot

---
+++ src/test/java/com/uttam/kafka/KafkaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttam.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
