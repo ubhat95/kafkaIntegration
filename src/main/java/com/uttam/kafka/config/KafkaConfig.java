7091d68feeedb4c56b758cd99fb1ea962a3e2ada
Uttam Bhat <uttambhat1711@gmail.com>
2023-11-21 18:08:47 -0500

    producer

---
+++ src/main/java/com/uttam/kafka/config/KafkaConfig.java
@@ -0,0 +1,61 @@
+package com.uttam.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@Configuration
+public class KafkaConfig {
+	
+	@Value("${kafka.bootstrap.servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.key.serializer}")
+    private String keySerializer;
+
+    @Value("${kafka.value.serializer}")
+    private String valueSerializer;
+
+    
+    @Bean
+    ConsumerFactory<String, Object> consumerFactory() {
+    Map<String, Object> configProps = new HashMap<>();
+    configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+    configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "default-task-group");
+    configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+    configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    return new DefaultKafkaConsumerFactory<>(configProps);
+  }
+
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+    return new DefaultKafkaProducerFactory<String, Object>(props);
+  }
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate() {
+    var kafkaTemplate = new KafkaTemplate<>(producerFactory());
+    kafkaTemplate.setConsumerFactory(consumerFactory());
+    return kafkaTemplate;
+  }
+}
--- src/main/java/com/uttam/kafka/producer/UserKafkaProducer.java
+++ src/main/java/com/uttam/kafka/producer/UserKafkaProducer.java
@@ -3,7 +3,6 @@ package com.uttam.kafka.producer;
 import java.util.HashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
