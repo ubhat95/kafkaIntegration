32838cd7b6276100cfd5e51392b28fe79cecf8b9
Uttam Bhat <53697553+ubhat95@users.noreply.github.com>
2023-11-21 12:51:34 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.uttam.kafkaintegration {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.uttam.kafkaintegration to javafx.fxml;
-    exports com.uttam.kafkaintegration;
-}
